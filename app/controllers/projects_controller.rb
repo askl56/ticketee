e9bf9bd86936c9f06d88f0dcb1007b5e43ddb3c0
askl56 <amscott@openmailbox.org>
2015-10-10 19:28:57 +0100

    you can now update a project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -30,9 +30,13 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
-    @project.update(project_params)
-    flash[:notice] = "Project has been updated."
-    redirect_to @project
+    if @project.update(project_params)
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated."
+      render "edit"
+    end
   end
 
   private
--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 feature "Editing Projects" do
-  scenario "Updating a project" do
+  before do
     FactoryGirl.create(:project, name: "TextMate 2")
     visit "/"
     click_link "TextMate 2"
     click_link "Edit Project"
+  end
+
+  scenario "Updating a project" do
     fill_in "Name", with: "TextMate 2 beta"
     click_button "Update Project"
     expect(page).to have_content("Project has been updated.")
