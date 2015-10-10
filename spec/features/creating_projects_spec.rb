b6b3c10b1ff4c8da4529d28215a49a43330a9305
askl56 <amscott@openmailbox.org>
2015-10-10 18:51:10 +0100

    Added validation to ensure names are specified when creating objects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,17 +1,27 @@
 require 'rails_helper'
 
+
 feature 'Creating Projects' do
-  scenario "can create a project" do
+
+  before do
     visit '/'
     click_link 'New Project'
+  end
+
+  scenario "can create a project" do
     fill_in 'Name', with: 'TextMate 2'
     fill_in 'Description', with: 'A text-editor for OS X'
     click_button 'Create Project'
     expect(page).to have_content('Project has been created')
-
     project = Project.where(name: "TextMate 2").first
     expect(page.current_url).to eql(project_url(project))
     title = "TextMate 2 - Projects - Ticketee"
     expect(page).to have_title(title)
+  end
+
+  scenario "can not create a project without a name" do
+    click_button 'Create Project'
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
   end
 end
