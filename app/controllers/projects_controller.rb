5f8e4e4368b57791ccf8406738e1269c89968978
askl56 <amscott@openmailbox.org>
2015-10-10 23:19:24 +0100

    Restrict access to project actions to admins only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 
+  before_action :authorize_admin!, except: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -58,5 +59,13 @@ class ProjectsController < ApplicationController
     flash[:alert] = "The project you were looking" +
       " for could not be found."
     redirect_to projects_path
+  end
+
+  def authorize_admin!
+    require_signin!
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,8 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
-require "support/authentication_helpers"
-
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
