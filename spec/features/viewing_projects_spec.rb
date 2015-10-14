9e5d283546ea257f1d2522d2802fb79c209b599f
askl56 <amscott@openmailbox.org>
2015-10-14 15:34:47 +0100

    added permission model

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,12 +1,17 @@
 require 'rails_helper'
 
 feature "Viewing projects" do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:project) { FactoryGirl.create(:project) }
 
-  scenario "Listing all projects" do
-    project = FactoryGirl.create(:project, name: "TextMate 2")
-    visit '/'
-    click_link 'TextMate 2'
-    expect(page.current_url).to eql(project_url(project))
+  before do
+    sign_in_as!(user)
+    define_permission!(user, :view, project)
   end
 
+  scenario "Listing all projects" do
+    visit '/'
+    click_link project.name
+    expect(page.current_url).to eql(project_url(project))
+  end
 end
